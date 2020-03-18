83329f304d4267378615f068030ec5e5c9e3639a
s1nglethr3ad <s1nglethr3ad@gmail.com>
2020-03-18 07:19:53 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" >> /etc/apt/sources.list
 gpg --keyserver pgpkeys.mit.edu --recv-key  ED444FF07D8D0BF6
 gpg -a --export ED444FF07D8D0BF6 | sudo apt-key add -
+export DEBIAN_FRONTEND=noninteractive
 apt-get -y update
-DEBIAN_FRONTEND=noninteractive apt-get upgrade -yq
+apt-get upgrade -yq
 apt-get -y dist-upgrade
 apt-get -y autoremove
 clear
