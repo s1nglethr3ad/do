6da3b7283e4ed0fd2ccdbebcddff1921a3ea35ce
s1nglethr3ad <s1nglethr3ad@gmail.com>
2020-03-19 19:36:10 -0400

    Update ubugui.tf

--- ubugui.tf
+++ ubugui.tf
@@ -1,12 +1,12 @@
 provider "digitalocean" {
-  token = "b5bf5f791c55118f00a80be947f505e528d371b6a8480b89e93ce566f755afda"
+  token = "INSERTDOAPITOKEN"
 }
 resource "digitalocean_droplet" "web" {
   name     = "ubuntu-tf-gui"
   image    = "ubuntu-18-04-x64"
   region   = "nyc3"
   size     = "s-4vcpu-8gb"
-  ssh_keys = [26847995]
+  ssh_keys = [SSH-ID]
   provisioner "remote-exec" {
     inline = [
       "wget https://raw.githubusercontent.com/s1nglethr3ad/do/master/ubugui.sh",
@@ -16,7 +16,7 @@ resource "digitalocean_droplet" "web" {
     connection {
       type        = "ssh"
       host        = "${self.ipv4_address}"
-      private_key = "${file("/Users/user/do_keys/id_rsa")}"
+      private_key = "${file("id_rsa")}"
       user        = "root"
       timeout     = "2m"
     }
