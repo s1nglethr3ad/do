9196f53c1b5e4218a13ab753ae6171938ab65464
s1nglethr3ad <s1nglethr3ad@gmail.com>
2020-03-19 20:11:37 -0400

    Update ubugui.sh

--- ubugui.sh
+++ ubugui.sh
@@ -1,6 +1,8 @@
 echo "SYSTEM UPDATE"
 export DEBIAN_FRONTEND=noninteractive
-apt-get update && sudo apt-get dist-upgrade -y 
+apt-get -y update
+apt-get upgrade -yq
+apt-get -y dist-upgrade
 apt-get install --no-install-recommends ubuntu-mate-core ubuntu-mate-desktop -y
 apt-get install mate-core mate-desktop-environment mate-notification-daemon xrdp -y
 echo "ADD BURP USER"
