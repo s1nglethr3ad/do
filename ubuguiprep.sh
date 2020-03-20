3f9daf58229ff245365ef4d7db2b844aa4da673d
s1nglethr3ad <s1nglethr3ad@gmail.com>
2020-03-20 12:57:40 -0400

    Update ubuguiprep.sh

--- ubuguiprep.sh
+++ ubuguiprep.sh
@@ -1,12 +1,13 @@
 echo "SYSTEM UPDATE"
 export DEBIAN_FRONTEND=noninteractive
 add-apt-repository universe
-useradd burp -p password
 sleep 5
-usermod -aG admin burp 
-usermod -aG sudo burp
 apt-get -y update
 sleep 30
 apt-get upgrade -yq
 sleep 30
 apt-get -y dist-upgrade
+sleep 30
+useradd burp -p password
+usermod -aG admin burp 
+usermod -aG sudo burp
