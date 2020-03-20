476aae8314dae5b68cd9c45da13dd94e754126ea
s1nglethr3ad <s1nglethr3ad@gmail.com>
2020-03-19 21:59:57 -0400

    Update ubugui.sh

--- ubugui.sh
+++ ubugui.sh
@@ -3,14 +3,9 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get -y update
 apt-get upgrade -yq
 apt-get -y dist-upgrade
-apt-get install --no-install-recommends ubuntu-mate-core ubuntu-mate-desktop -y
-apt-get install mate-core mate-desktop-environment mate-notification-daemon xrdp -y
-echo "ADD BURP USER"
+apt-get install -y ubuntu-desktop
+apt-get install xrdp
+systemctl enable xrdp
 useradd -p password burp
 usermod -aG admin burp 
 usermod -aG sudo burp
-su - burp
-echo "SETUP X11 AND XRDP" 
-echo mate-session> ~/.xsession
-cp /home/burp/.xsession /etc/skel
-service xrdp restart
