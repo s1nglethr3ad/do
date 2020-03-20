67b50f3964048f1cbfd23ff619fef1cfd5cd8d1e
s1nglethr3ad <s1nglethr3ad@gmail.com>
2020-03-19 22:26:48 -0400

    Update ubugui.sh

--- ubugui.sh
+++ ubugui.sh
@@ -1,9 +1,13 @@
 echo "SYSTEM UPDATE"
 export DEBIAN_FRONTEND=noninteractive
 apt-get -y update
+sleep 30
 apt-get upgrade -yq
+sleep 30
 apt-get -y dist-upgrade
+sleep 30
 apt-get install -y ubuntu-desktop
+sleep 30
 apt-get install -y xrdp
 systemctl enable xrdp
 useradd -p password burp
