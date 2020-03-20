c81a35c92f262ac0af8b085ad79e268cb91012dc
s1nglethr3ad <s1nglethr3ad@gmail.com>
2020-03-20 10:17:00 -0400

    Update ubugui.sh

--- ubugui.sh
+++ ubugui.sh
@@ -1,18 +1,12 @@
 echo "SYSTEM UPDATE"
 export DEBIAN_FRONTEND=noninteractive
 add-apt-repository universe
+'useradd burp; echo -e "password\npassword" | passwd burp'
 sleep 5
+usermod -aG admin burp 
+usermod -aG sudo burp
 apt-get -y update
 sleep 30
 apt-get upgrade -yq
 sleep 30
 apt-get -y dist-upgrade
-sleep 30
-apt install -y xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils
-#apt-get install -y ubuntu-desktop
-#sleep 30
-#apt-get install -y xrdp
-#systemctl enable xrdp
-#'useradd burp; echo -e "password\npassword" | passwd burp'
-#usermod -aG admin burp 
-#usermod -aG sudo burp
