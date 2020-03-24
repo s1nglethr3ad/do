575a4d0afb7f4ecdf605b246dbef4d7736f99425
s1nglethr3ad <s1nglethr3ad@gmail.com>
2020-03-24 07:42:05 -0400

    Create ubuphish.tf

---
+++ ubuphish/ubuphish.tf
@@ -0,0 +1,23 @@
+provider "digitalocean" {
+  token = "APITOKEN"
+}
+resource "digitalocean_droplet" "web" {
+  name     = "phish"
+  image    = "ubuntu-18-04-x64"
+  region   = "nyc3"
+  size     = "s-1vcpu-2gb"
+  ssh_keys = [26847995]
+  provisioner "remote-exec" {
+    inline = [
+      "wget https://raw.githubusercontent.com/s1nglethr3ad/do/master/ubuphish/ubuphish.sh",
+      "chmod +x ubuphish.sh",
+    ]
+    connection {
+      type        = "ssh"
+      host        = "${self.ipv4_address}"
+      private_key = "${file("id_rsa")}"
+      user        = "root"
+      timeout     = "2m"
+    }
+  }
+}
