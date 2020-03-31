409bdbc6b3f718b2ecb86187e54e164e98f79dc2
s1nglethr3ad <s1nglethr3ad@gmail.com>
2020-03-31 10:03:51 -0400

    Update phishprox.tf

--- phishprox/phishprox.tf
+++ phishprox/phishprox.tf
@@ -1,23 +1,38 @@
 provider "digitalocean" {
   token = "APITOKEN"
 }
+
 resource "digitalocean_droplet" "phishprox" {
   name     = "phishprox"
   image    = "ubuntu-18-04-x64"
   region   = "nyc3"
   size     = "s-1vcpu-1gb"
-  ssh_keys = [SSH-ID]
+  ssh_keys = [26847995]
   provisioner "file" {
     source      = "./configs/modlishka.json"
     destination = "/root/modlishka.json"
+    
     connection {
       type        = "ssh"
-      host        = "${self.ipv4_address}"
-      private_key = "${file("/Users/user/.ssh/id_rsa")}"
+      host        = self.ipv4_address
+      private_key = file("id_rsa")
       user        = "root"
       timeout     = "2m"
     }
-}
+  }
+  provisioner "file" {
+    source      = "./configs/certbot.sh"
+    destination = "/root/certbot.sh"
+    
+    connection {
+      type        = "ssh"
+      host        = self.ipv4_address
+      private_key = file("id_rsa")
+      user        = "root"
+      timeout     = "2m"
+    }
+  }
+
   provisioner "remote-exec" {
     inline = [
       "export DEBIAN_FRONTEND=noninteractive",
@@ -25,53 +40,27 @@ resource "digitalocean_droplet" "phishprox" {
       "apt-get -y update",
       "apt-get upgrade -yq",
       "apt-get -y dist-upgrade",
-      "apt-get install -y certbot",
       "wget https://github.com/drk1wi/Modlishka/releases/download/v.1.1.0/Modlishka-linux-amd64",
-      "chmod +x Modlishka-linux-amd64 ; ls -lah",
-      #"certbot certonly --manual --preferred-challenges=dns --server https://acme-v02.api.letsencrypt.org/directory --agree-tos -d *.crimsonbyte.net --email noreply@live.com --non-interactive",
-    ]
+      "chmod +x Modlishka-linux-amd64",
+      "sleep 60",
+      "apt-get install -y python-pip",
+      "sleep 30",
+      "pip install certbot-dns-digitalocean",
+      "sleep 30",
+      "chmod +x /root/certbot.sh",
+      "chmod +x /root/modlishka.json",
+      "chmod +x /root/cert_to_modlishka.sh",
+      "./certbot.sh",
+      "sleep 60",
+      "./cert_to_modlishka.sh",
+      ]
+    
     connection {
       type        = "ssh"
-      host        = "${self.ipv4_address}"
-      private_key = "${file("id_rsa")}"
+      host        = self.ipv4_address
+      private_key = file("id_rsa")
       user        = "root"
       timeout     = "2m"
     }
-  }
-}
-
-provider "acme" {
-  server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
-}
-
-resource "tls_private_key" "private_key" {
-  algorithm = "RSA"
-}
-
-resource "acme_registration" "reg" {
-  account_key_pem = tls_private_key.private_key.private_key_pem
-  email_address   = "noreply@live.com"
-}
-
-resource "tls_private_key" "cert_private_key" {
-  algorithm = "RSA"
-}
-
-resource "tls_cert_request" "req" {
-  key_algorithm   = "RSA"
-  private_key_pem = tls_private_key.cert_private_key.private_key_pem
-  dns_names       = ["*.yourdomain.com"]
-
-  subject {
-    common_name = "yourdomain"
-  }
-}
-
-resource "acme_certificate" "certificate" {
-  account_key_pem         = acme_registration.reg.account_key_pem
-  certificate_request_pem = tls_cert_request.req.cert_request_pem
-
-  dns_challenge {
-    provider = "route53"
   }
 }
