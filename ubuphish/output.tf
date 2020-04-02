b1e2d1e85a897b48ce70bc22ce73a4ced8a3efa2
s1nglethr3ad <s1nglethr3ad@gmail.com>
2020-04-01 21:04:06 -0400

    Update output.tf

--- ubuphish/output.tf
+++ ubuphish/output.tf
@@ -2,6 +2,14 @@ output "Name" {
   value = "${digitalocean_droplet.phish.name}"
 }
 
+output "Cert_ID" {
+  value = "Cert ID is ${digitalocean_certificate.cert.id}"
+}
+
 output "outputs" {
   value = "phish IP is ${digitalocean_droplet.phish.ipv4_address}"
 }
+# Uncomment to display load-balancer IP at the end of build
+#output "LB_IP" {
+#  value = "Load Balancer IP is ${digitalocean_loadbalancer.public.ip}"
+#}
